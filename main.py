e1a961148cdc66a428875f22b6386cebca4a87ca
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-08 12:49:25 +0500

    op

---
+++ main.py
@@ -0,0 +1,9 @@
+mark1 = 10
+mark2 = 30
+mark3 = 60
+mark4 = 80
+mark5 = 100
+sum = mark1+mark2+mark3+mark4+mark5
+print("the sum of all 5 marks is:",sum)
+average = sum/5
+print("the average of all the mark is:",average)
