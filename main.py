fe761177bde831bb1d2529170667716f388804a1
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-08 13:06:45 +0500

    op1

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ sum = mark1+mark2+mark3+mark4+mark5
 print("the sum of all 5 marks is:",sum)
 average = sum/5
 print("the average of all the mark is:",average)
+amount = int(input("please enter amount for withdraw:"))
+note1 = amount//100
+note2 = (amount%100)//50
+note3 = ((amount%100)%50)//10
+print ("notes of 100 ruppes" , note1)
+print ("notes of 50 rupees" , note2)
+print ("notes of 10 rupees" , note3)
